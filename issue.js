89757ee0cf79c726da9865955cabb0863bc7caa8
Topppy <taoyiming2010@126.com>
2022-12-30 19:02:40 +0800

    fix: tocken env

--- issue.js
+++ issue.js
@@ -5,10 +5,12 @@ const Hexo =require('hexo');
 const MILESTONE_PUBLISH = 'publish';
 
 try {
-    const issueUrl = core.getInput('issue_url');
+    // const issueUrl = core.getInput('issue_url');
+    const issueUrl = process.env.issueUrl
     const index = issueUrl.indexOf('/repos');
     const endpoint = issueUrl.substring(index);
-    const token = core.getInput('token');
+    // const token = core.getInput('token');
+    const token = process.env.token
 
     const gh = new Octokit({ auth: token });
 
