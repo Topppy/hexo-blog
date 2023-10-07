1020c4d55abcd767afa38ea79b5892772f8ec632
Topppy <taoyiming2010@126.com>
2023-10-07 15:23:13 +0800

    debug: post

--- issue.js
+++ issue.js
@@ -1,17 +1,15 @@
 const core = require('@actions/core');
 const { Octokit } = require('octokit');
-const Hexo =require('hexo');
+const Hexo = require('hexo');
 
 const MILESTONE_PUBLISH = 'publish';
 
 try {
     console.log(`issueUrl: ${process.env.issueUrl}`);
-    console.log(`token: ${process.env.issueToken}`);
-    // const issueUrl = core.getInput('issue_url');
+    // console.log(`token: ${process.env.issueToken}`);
     const issueUrl = process.env.issueUrl
     const index = issueUrl.indexOf('/repos');
     const endpoint = issueUrl.substring(index);
-    // const token = core.getInput('token');
     const token = process.env.issueToken
 
     const gh = new Octokit({ auth: token });
@@ -21,16 +19,19 @@ try {
         console.log(`Converting issue ${endpoint} to Hexo post...`);
         gh.request(`GET ${endpoint}`).then((response) => {
             const { title, updated_at: date, labels, milestone, body: content } = response.data;
+            const filePath = `source/_posts/${title.replace(/\s+/g, '-')}.md}`
             console.log('title:', title);
             console.log('date:', date);
             console.log('labels:', labels);
             console.log('content:', content);
+            console.log('filePath:', filePath);
             if (milestone.title !== MILESTONE_PUBLISH) {
                 console.log(`Issue does not have milestone ${MILESTONE_PUBLISH}`);
             } else {
                 const tags = labels.map((label) => label.name);
                 hexo.post.create({
                     title,
+                    path: filePath,
                     date,
                     tags,
                     content,
